511f0898b88b47cf532d6575e49f75cac44b15ab
Vitaliy <kevush_vitaliy@mail.ru>
2019-11-15 20:52:50 +0600

    Commit 1

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
 
     public static void main(String[] args) {
-	ExampleOne exampleOne = new ExampleOne();
+
+    	ExampleOne exampleOne = new ExampleOne();
 	exampleOne.start();
+
 
 	ExampleTwo exampleTwo = new ExampleTwo();
 	exampleTwo.start();
+
 
 	ExampleThree exampleThree = new ExampleThree();
 	exampleThree.start();
